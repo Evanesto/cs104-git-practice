27a93faa2b9366a0b8e57d8b83180455723a8238
Evan Zamora <egzamora@usc.edu>
2019-01-12 20:52:33 -0800

    updating from VM

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,4 +5,5 @@ int main()
   std::cout << "FightOn!" << std::endl; // was HelloWorld!
   std::cout << "\nLook, more text!";
   std::cout << "\nAnd another line.";
+  std::cout << "\nThis time modified from the VM"
 }
